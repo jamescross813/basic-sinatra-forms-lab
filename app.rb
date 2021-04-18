79cab46be5e335fcb141c750f8ad2c452ce321c3
jamescross <crossjames813@gmail.com>
2021-04-18 13:06:52 -0500

    commit tree 610013ae4af6c90fa8f9e37fa1ce7abcb40e8d32

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/team' do
+        @team_name = params[:name]
+        @coach = params[:coach]
+        @point = params[:pg]
+        @shoot = params[:sg]
+        @small = params[:sf]
+        @power = params[:pf]
+        @center = params[:c]
+        erb :team
+    end
 
 end
